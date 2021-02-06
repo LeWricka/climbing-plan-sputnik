baecb62137fcb5bd8c249eafd500053208519a99
Iker Mariñelarena <icka69@gmail.com>
2021-02-06 21:55:19 +0100

    [] - Implement complex request

--- index.js
+++ index.js
@@ -11,19 +11,25 @@ exports.sputnik1 = (req, res) => {
 };
 
 function sputnik1(req, res) {
-    let materials = req.body.materials
-    let routinesBase = {'climbing_gym': ['climbing_gym_3', 'climbing_gym_2'], 'trx': ["trx_ring"]}
-    let routines = []
-    materials.forEach(material => {
-        routines.push(routinesBase[material])
+    let userResponse = req.body.lastResponseInJson
+    let equipmentToTrainingMapper = req.body.materialsToRoutinesMapper
+    // let routinesBase = {'climbing_gym': ['climbing_gym_3', 'climbing_gym_2'], 'trx': ["trx_ring"]}
+    let routinesForTrainings = []
+    let availableEquipments =  userResponse.availableEquipment.split(',')
+    availableEquipments.forEach(userTrainingEquipment => {
+        equipmentToTrainingMapper.forEach(equipmentToTrainingMapper => {
+            // res.status(200).send(userTrainingEquipment +': '+equipmentToTrainingMapper.equipment1)
+
+            // res.status(200).send(equipmentToTrainingMapper.equipment1  +' ' + trainingMaterial)
+            if(userTrainingEquipment = equipmentToTrainingMapper.equipment1) {
+                routinesForTrainings.push(equipmentToTrainingMapper.routine)
+            }
+        })
     })
-    res.status(200).send(`<!doctype html>
-    <head>
-      <title>Your routine</title>
-    </head>
-    <body>
-      ${routines}
-    </body>
-  </html>`);
+    res.status(200).send(routinesForTrainings)
+    let trimmedRoutinesForTrainings = [...new Set(routinesForTrainings)];
+
+
+    res.status(200).send(trimmedRoutinesForTrainings);
 }
 
